57426d41b6035f3293199337b207035bbf337311
Vlasov Alex <Vlas442004@gmail.com>
2025-07-03 22:18:35 +0500

    разместил кнопки

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import pandas as pd
 
-# Глобальные переменные для хранения путей к файлам
 file_paths = [None, None]
 
 
@@ -16,11 +15,9 @@ def load_file(button_number):
     filename = filedialog.askopenfilename(title=f"Выберите файл {button_number}", filetypes=filetypes)
     if filename:
         file_paths[button_number - 1] = filename
-        #messagebox.showinfo("Файл выбран", f"Файл {button_number}:\n{filename}")
 
 
 def read_data(file_path):
-    # Определяем тип файла по расширению и читаем его
     if not file_path:
         return None
     ext = file_path.split('.')[-1].lower()
@@ -51,11 +48,9 @@ def compare_files():
     if df1 is None or df2 is None:
         return
 
-    # Пример сравнения: ищем общие строки по всем столбцам
     common_rows = pd.merge(df1, df2, how='inner')
 
-    # Записываем результат в файл
-    output_file = "comparison_result.xlsx"
+    output_file = filedialog.asksaveasfilename() #"comparison_result.xlsx"
     try:
         common_rows.to_excel(output_file, index=False)
         messagebox.showinfo("Готово", f"Результат сохранен в {output_file}")
@@ -63,17 +58,28 @@ def compare_files():
         messagebox.showerror("Ошибка записи файла", str(e))
 
 
-# Создаем интерфейс
-root = tk.Tk()
-root.title("Сравнение файлов")
-root.geometry("400x200")
+if __name__ == "__main__":
 
-btn_load1 = tk.Button(root, text="Загрузить 1 файл", command=lambda: load_file(1))
-btn_load2 = tk.Button(root, text="Загрузить 2 файла", command=lambda: load_file(2))
-btn_compare = tk.Button(root, text="Сравнить файлы и сохранить результат", command=compare_files)
+    root = tk.Tk()
+    root.title("Сравнение файлов")
+    root.geometry("500x200+700+400")
 
-btn_load1.pack(pady=10)
-btn_load2.pack(pady=10)
-btn_compare.pack(pady=20)
+    top_frame = tk.Frame(root)
+    middle_frame = tk.Frame(root)
+    bottom_frame = tk.Frame(root)
 
-root.mainloop()
+    top_frame.pack(side="top", fill="x", pady=10)
+    bottom_frame.pack(side="bottom", fill="x", pady=10)
+
+    btn_load1 = tk.Button(top_frame, text="Загрузить реестр 1", command=lambda: load_file(1))
+    btn_load2 = tk.Button(top_frame, text="Загрузить реестр 2", command=lambda: load_file(2))
+    btn_compare = tk.Button(bottom_frame, text="Сравнить файлы и сохранить результат", command=compare_files)
+
+    btn_load1.pack(side='left', padx=40, ipadx=10, ipady=10)
+    btn_load2.pack(side='right', padx=40, ipadx=10, ipady=10)
+    btn_compare.pack(side="bottom", fill="x", pady=20)
+
+    bottom_frame.pack_propagate(False)
+    bottom_frame.configure(height=60)
+
+    root.mainloop()
